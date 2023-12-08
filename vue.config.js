acf60c0f7467dfbe3cc3c7766d2eda3abf17f714
daryl <livesaex@gmail.com>
2023-12-08 17:25:06 +0800

    fix: Invalid options object. options has an unknown property 'before' 的问题处理，将before 替换为 onBeforeSetupMiddleware。

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+const registerRouter = require('./backend/router')
+
+// Vue CLI 3.x 及更高版本中，使用 defineConfig 够提供更好的类型提示和类型检查，更容易地查看配置选项和避免一些潜在的错误。
 module.exports = defineConfig({
-  transpileDependencies: true,
+  // 关闭设置：所有的依赖模块都会被 Babel 转译
+  transpileDependencies: false,
   css: {
     loaderOptions: {
       sass: {
@@ -9,6 +13,16 @@ module.exports = defineConfig({
           @import "@/assets/scss/variable.scss";
           @import "@/assets/scss/mixin.scss";
         `
+      }
+    }
+  },
+  devServer: {
+    // before方法已过时，参考https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md
+    onBeforeSetupMiddleware(devServer) {
+      if (typeof registerRouter === 'function') {
+        registerRouter(devServer.app)
+      } else {
+        console.error('registerRouter is not a function')
       }
     }
   }
